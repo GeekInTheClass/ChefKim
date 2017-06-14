c64668c45f81141490a793907e7525b1b31b1883
kwang5348 <kwang5348@gmail.com>
2017-06-14 23:58:23 +0900

    DummyData update

--- Recipick/Recipick/Recipe_Data.swift
+++ Recipick/Recipick/Recipe_Data.swift
@@ -14,7 +14,7 @@ var g_RecipeArray = [Recipe]()
 
 // 소고기 무국
 
-let Beefbureau = Recipe(id: "0001",
+let Beefbureau = Recipe(id: "1",
                         name: "소고기무국",
                         ingrediant: [Recipe.Ingrediant.Beef],
                         time: Recipe.Time.H1,
@@ -23,7 +23,7 @@ let Beefbureau = Recipe(id: "0001",
                         recipe_ingrediant: "test",
                         recipe_contents: "test")
 //어묵국
-let Fishpaste = Recipe(id: "0002",
+let Fishpaste = Recipe(id: "2",
                        name: "어묵국",
                        ingrediant: [Recipe.Ingrediant.FishCake],
                        time: Recipe.Time.H1,
@@ -34,7 +34,7 @@ let Fishpaste = Recipe(id: "0002",
 
 
 //야채피클
-let PickledCucumber = Recipe(id: "0003",
+let PickledCucumber = Recipe(id: "3",
                              name: "야채피클",
                              ingrediant: [Recipe.Ingrediant.Vege],
                              time: Recipe.Time.M30,
@@ -43,7 +43,7 @@ let PickledCucumber = Recipe(id: "0003",
                              recipe_ingrediant: "test",
                              recipe_contents: "test")
 //김치볶음밥
-let KimchiFriedRice = Recipe(id: "0004",
+let KimchiFriedRice = Recipe(id: "4",
                              name: "김치볶음밥",
                              ingrediant: [Recipe.Ingrediant.Kimchi, Recipe.Ingrediant.Egg, Recipe.Ingrediant.Pork],
                              time: Recipe.Time.M30,
@@ -53,7 +53,7 @@ let KimchiFriedRice = Recipe(id: "0004",
                              recipe_contents: "test")
 
 //바지락 순두부찌개
-let ClamSoftTofuStew = Recipe(id: "0005",
+let ClamSoftTofuStew = Recipe(id: "5",
                               name: "바지락 순두부찌개",
                               ingrediant: [Recipe.Ingrediant.Clam, Recipe.Ingrediant.Egg],
                               time: Recipe.Time.H1,
@@ -63,7 +63,7 @@ let ClamSoftTofuStew = Recipe(id: "0005",
                               recipe_contents: "test")
 
 //제육볶음
-let FriedSpicyPork = Recipe(id: "0006",
+let FriedSpicyPork = Recipe(id: "6",
                             name: "재육볶음",
                             ingrediant: [Recipe.Ingrediant.Pork],
                             time: Recipe.Time.H3,
@@ -73,7 +73,7 @@ let FriedSpicyPork = Recipe(id: "0006",
                             recipe_contents: "test")
 
 //가지볶음
-let FriedEggPlant = Recipe(id: "0007",
+let FriedEggPlant = Recipe(id: "7",
                            name: "가지볶음",
                            ingrediant: [Recipe.Ingrediant.Vege],
                            time: Recipe.Time.H1,
@@ -83,7 +83,7 @@ let FriedEggPlant = Recipe(id: "0007",
                            recipe_contents: "test")
 
 //봉골레파스타
-let VongolePasta = Recipe(id: "0008",
+let VongolePasta = Recipe(id: "8",
                           name: "봉골레파스타",
                           ingrediant: [Recipe.Ingrediant.Clam],
                           time: Recipe.Time.H1,
@@ -93,7 +93,7 @@ let VongolePasta = Recipe(id: "0008",
                           recipe_contents: "test")
 
 //소고기미역국
-let BeefSeaweedSoup = Recipe(id: "0009",
+let BeefSeaweedSoup = Recipe(id: "9",
                              name: "소고기미역국",
                              ingrediant: [Recipe.Ingrediant.Beef],
                              time: Recipe.Time.M30,
@@ -101,4 +101,13 @@ let BeefSeaweedSoup = Recipe(id: "0009",
                              category: Recipe.Category.Korean,
                              recipe_ingrediant: "test",
                              recipe_contents: "test")
+//매운감자조림
+/*let Beefbureau = Recipe(id: "10",
+                        name: "매운감자조림",
+                        ingrediant: [Recipe.Ingrediant.Beef],
+                        time: Recipe.Time.H1,
+                        situation: Recipe.Situation.Home,
+                        category: Recipe.Category.Korean,
+                        recipe_ingrediant: "test",
+                        recipe_contents: "test")*/
 
